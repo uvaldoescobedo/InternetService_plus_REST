74e66b1dd5eebee202d7dda4460fbca7ec3b4a9f
uescobedo <uescobedo@boz.mx>
2020-10-29 13:46:55 -0600

    prueba de sincronizar clasificadores completada OK

--- app/src/main/java/com/example/internetservice_plus_rest/MainActivity.kt
+++ app/src/main/java/com/example/internetservice_plus_rest/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.internetservice_plus_rest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.internetservice_plus_rest.core.RetrofitAPI
+import com.example.internetservice_plus_rest.repository.DedecRepository
+import com.example.internetservice_plus_rest.viewModels.SyncVMFunctions
+import com.example.internetservice_plus_rest.viewModels.SyncVMPFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var modelFuntions : SyncVMFunctions
+    lateinit var factory : SyncVMPFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        factory = SyncVMPFactory(DedecRepository(RetrofitAPI().getRetrofitApi()))
+        modelFuntions = ViewModelProvider(this,factory!!).get(SyncVMFunctions::class.java)
+       // observerData()
+
+        modelFuntions.syncClasificadores("x9h3sl",false)
+        observerData()
+    }
+
+    private fun observerData() {
+        modelFuntions.synState.observe(this,  Observer {
+            Log.i("MainActivity synState ",it.toString())
+        })
+        modelFuntions.clasificadoresSuccessResponse.observe(this,  Observer {
+            Log.i("MainActivity Success ",it.toString())
+        })
+        modelFuntions.clasificadoresBadResponse.observe(this,  Observer {
+            Log.i("MainActivity Bad ",it.toString())
+        })
+
     }
 }
